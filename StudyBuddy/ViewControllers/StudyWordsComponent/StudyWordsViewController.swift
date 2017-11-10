370fd6fe342b5d06d43fe79981a2a7dda5c2fb6c
Peter <peter.aguilar2287@gmail.com>
2017-11-10 12:03:27 -0500

    add going back functionality

--- StudyBuddy/ViewControllers/StudyWordsComponent/StudyWordsViewController.swift
+++ StudyBuddy/ViewControllers/StudyWordsComponent/StudyWordsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class StudyWordsViewController: UIViewController {
     var wordsToStudy = [FlashCardsTableViewController.wordsObject]()
     var counter = 0
+    var counterBehindOne = 0
     @IBOutlet weak var flashCardOutlet: UILabel!
     @IBOutlet weak var secondInDeckFlashCardOutlet: UILabel!
     
@@ -25,27 +26,18 @@ class StudyWordsViewController: UIViewController {
         
         flashCardOutlet.text = wordsToStudy[counter].translated
         secondInDeckFlashCardOutlet.text = wordsToStudy[counter + 1].translated
-        
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipedRight))
-        swipeRight.direction = UISwipeGestureRecognizerDirection.right
-        self.view.addGestureRecognizer(swipeRight)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.view.sendSubview(toBack: self.moveBackInDeckOutlet)
-        self.moveBackInDeckOutlet.center.x -= self.view.bounds.width
-
-
+        //        self.moveBackInDeckOutlet.center.x -= self.view.bounds.width
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @objc
-    func swipedRight() {
-        print("right on")
     }
     
     @objc
@@ -55,42 +47,61 @@ class StudyWordsViewController: UIViewController {
         flashCardOutlet.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
         
         let xFromCenter = view.bounds.width / 2 - flashCardOutlet.center.x
-        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
-        let scale = min(100 / abs(xFromCenter), 1)
-        var scaledAndRotated = rotation.scaledBy(x: scale, y: scale)
-        
-        flashCardOutlet.transform = scaledAndRotated
+//        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+//        let scale = min(100 / abs(xFromCenter), 1)
+//        var scaledAndRotated = rotation.scaledBy(x: scale, y: scale)
+
+//        flashCardOutlet.transform = scaledAndRotated
         
         // have to make the transition animation happen...
-        if gestureRec.velocity(in: self.view).x > 0 {
         
-         
-            rotation = CGAffineTransform(rotationAngle: 0)
-            scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
-            flashCardOutlet.transform = scaledAndRotated
+        if gestureRec.velocity(in: self.view).x > 0 {
+//            rotation = CGAffineTransform(rotationAngle: 0)
+//            scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
+//            flashCardOutlet.transform = scaledAndRotated
             flashCardOutlet.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+            if counter != 0 {
+            counter = counterBehindOne
+                
+                if (counter + 1) != wordsToStudy.count {
+                    secondInDeckFlashCardOutlet.text = wordsToStudy[counter + 1].translated
+                }
+            
+            self.moveBackInDeckOutlet.center.x -= self.view.bounds.width
+            
+      
             self.view.bringSubview(toFront: moveBackInDeckOutlet)
-
-            UIView.animate(withDuration: 2.0) {
+            
+            UIView.animate(withDuration: 1.0) {
+                self.moveBackInDeckOutlet.text = self.wordsToStudy[self.counterBehindOne].translated
                 self.moveBackInDeckOutlet.center = CGPoint(x: self.view.bounds.width / 3, y: self.view.bounds.height / 2)
-
                 self.moveBackInDeckOutlet.center.x += self.view.bounds.width / 4
             }
             
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
-                 self.moveBackInDeckOutlet.center.x -= self.view.bounds.width
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+                
+                self.flashCardOutlet.text = self.wordsToStudy[self.counter].translated
+                
+                self.moveBackInDeckOutlet.center.x -= self.view.bounds.width
                 self.view.sendSubview(toBack: self.moveBackInDeckOutlet)
-
             })
-           
+            }
         }
         
+     
+
         if gestureRec.state == .ended {
+            if counterBehindOne != 0 {
+            counterBehindOne = counter - 1
+            }
+            print("counter \(counter)")
+            print("counterbeh \(counterBehindOne)")
+            
             
             func resetCardCenter() {
-                rotation = CGAffineTransform(rotationAngle: 0)
-                scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
-                flashCardOutlet.transform = scaledAndRotated
+//                rotation = CGAffineTransform(rotationAngle: 0)
+//                scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
+//                flashCardOutlet.transform = scaledAndRotated
                 flashCardOutlet.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
             }
             
@@ -99,35 +110,17 @@ class StudyWordsViewController: UIViewController {
                 if counter < wordsToStudy.count - 1 {
                     //moves deck forward
                     counter = counter + 1
+                    counterBehindOne = counter - 1
                     if (counter + 1) != wordsToStudy.count {
-                        secondInDeckFlashCardOutlet.text = wordsToStudy[counter + 1].translated as? String
+                        secondInDeckFlashCardOutlet.text = wordsToStudy[counter + 1].translated
                     }
-                    flashCardOutlet.text = wordsToStudy[counter].translated as? String
-
-                }
-                resetCardCenter()
-            }
-            if flashCardOutlet.center.x > (view.bounds.width / 2 + 100) {
-                //moves deck back
-                if counter > 0 {
-                    counter = counter - 1
-                    flashCardOutlet.text = wordsToStudy[counter].translated as? String
+                    flashCardOutlet.text = wordsToStudy[counter].translated
                 }
                 resetCardCenter()
             }
             
-            if flashCardOutlet.center.y < (view.bounds.height / 2 - 100) {
-                //moves card to learned deck
-                if counter < wordsToStudy.count - 1 {
-                    counter = counter + 1
-                    if (counter + 1) != wordsToStudy.count {
-                        secondInDeckFlashCardOutlet.text = wordsToStudy[counter + 1].translated as? String
-                    }
-                    
-                        resetCardCenter()
-                        self.flashCardOutlet.text = self.wordsToStudy[self.counter].translated as? String
-                    
-                }
+            if gestureRec.velocity(in: self.view).y > 0 {
+                print("what the")
             }
         }
     }
